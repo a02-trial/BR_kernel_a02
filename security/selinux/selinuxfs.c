bbd2b98f4f14bb2232d8b8056cdc12a2e93f5a6a
a02-trial <cipin23@gmail.com>
2025-11-12 19:57:27 +0700

    Update selinuxfs.c

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -180,6 +180,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	new_value = !!new_value;
 
 	old_value = enforcing_enabled(state);
+	new_value = 0;
 	if (new_value != selinux_enforcing) { // SEC_SELINUX_PORTING_COMMON Change to use RKP
 		length = avc_has_perm(&selinux_state,
 				      current_sid(), SECINITSID_SECURITY,
